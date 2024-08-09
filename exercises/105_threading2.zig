31e539499aba4c4b85e7811f69fa7efa7bd49616
grandmasponge <tobyr2006@gmail.com>
2024-08-09 17:12:56 +0100

    end of zigling ..for now

--- exercises/105_threading2.zig
+++ exercises/105_threading2.zig
@@ -81,14 +81,21 @@ pub fn main() !void {
         defer handle1.join();
 
         // Second thread to calculate the minus numbers.
-        ???
-        
+        const handle2 = try std.Thread.spawn(.{}, thread_pi_minus, .{ &pi_minus, 3, count });
+        defer handle2.join();
     }
     // Here we add up the results.
     std.debug.print("PI ≈ {d:.8}\n", .{4 + pi_plus - pi_minus});
 }
 
 fn thread_pi(pi: *f64, begin: u64, end: u64) !void {
+    var n: u64 = begin;
+    while (n < end) : (n += 4) {
+        pi.* += 4 / @as(f64, @floatFromInt(n));
+    }
+}
+
+fn thread_pi_minus(pi: *f64, begin: u64, end: u64) !void {
     var n: u64 = begin;
     while (n < end) : (n += 4) {
         pi.* += 4 / @as(f64, @floatFromInt(n));
